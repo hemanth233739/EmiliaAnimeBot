f14cf6483208e0a8fa8ddd9bc97f12114a589d0f
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-09-12 11:28:05 +0530

    Update fuck.py

--- EmiliaAnimeBot/modules/fuck.py
+++ EmiliaAnimeBot/modules/fuck.py
@@ -45,35 +45,6 @@ async def is_register_admin(chat, user):
         return None
 
 
-@register(pattern="^/gs (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    if event.is_group:
-        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-            await event.reply(
-                " Hai.. You are not admin..  You can't use this command.. But you can use in my pm"
-            )
-            return
-    # SHOW_DESCRIPTION = False
-    input_str = event.pattern_match.group(
-        1
-    )  # + " -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" (mkv|mp4|avi|epub|pdf|mp3)"
-    input_url = "https://bots.shrimadhavuk.me/search/".format(input_str)
-    headers = {"USER-AGENT": "UniBorg"}
-    response = requests.get(input_url, headers=headers).json()
-    output_str = " "
-    for result in response["results"]:
-        text = result.get("title")
-        url = result.get("url")
-        description = result.get("description")
-        last = html2text.html2text(description)
-        output_str += "[{}]({})\n{}\n".format(text, url, last)
-    await event.reply(
-        "{}".format(output_str), link_preview=False, parse_mode="Markdown"
-    )
-
-
 @register(pattern="^/img (.*)")
 async def img_sampler(event):
     if event.fwd_from:
